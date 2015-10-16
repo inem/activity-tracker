a001c48e89b567785f0e3e3ccce2b2cb424cefe2
Alexander <x48j35@gmail.com>
2015-10-16 20:28:48 +0300

    Ability to add user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
+
+  validates :name, :repo, presence: true
+
+
   def tasks
     github = Github.new user: name, repo: repo
-    # github.repos.tags.each do |tag|
-    #   p (github.repos.commits.get "justcxx", "skillgrid", tag.commit.sha).commit.committer.date
-    # end
-    # github.repos.tags
-    []
+    github.repos.tags.each do |tag|
+       #(github.repos.commits.get name, name, tag.commit.sha).commit.committer.date
+    end
+    github.repos.tags
   end
 end
