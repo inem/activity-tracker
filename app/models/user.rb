a5249172955960d89f811cc4b2bb22d123942f36
Alexander <x48j35@gmail.com>
2015-10-16 20:52:42 +0300

    some styling, tag links

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,20 @@ class User < ActiveRecord::Base
   validates :name, :repo, presence: true
   has_many :exercises
 
-
   def tasks
     github = Github.new user: name, repo: repo
     github.repos.tags.each do |tag|
        #(github.repos.commits.get name, name, tag.commit.sha).commit.committer.date
     end
     github.repos.tags
+    binding.pry
   end
 
   private
 
   def init_exercises
-    self.tasks.each do |t|
-      self.exercises.create(name: t.name)
+    tasks.each do |t|
+      exercises.create(name: t.name)
     end
   end
-
 end
