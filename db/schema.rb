f30ab963ba1f874a46850e257c33f7883a307603
Alexander <x48j35@gmail.com>
2015-10-18 19:39:08 +0300

    Datetime for exercises

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016173033) do
+ActiveRecord::Schema.define(version: 20151018155534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20151016173033) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.datetime "start_date"
   end
 
   add_index "exercises", ["user_id"], name: "index_exercises_on_user_id", using: :btree
