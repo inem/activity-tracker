7f38e6a40453a6879c4633251f5867b9f53be083
Alexander <x48j35@gmail.com>
2015-10-20 18:05:01 +0300

    Main page link, seeds update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@ users = {
 users.each do |name, repo|
   User.create(name: name, repo: repo)
 end
+
+repos = ["edavis10/redmine", "justCxx/sicp"]
+
+repos.each do |repo|
+  Project.create(repo: repo)
+end
