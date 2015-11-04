92e89f0682ea4aa65b8f91c3c595ddb7abc20a82
qq1 <srglbnv@gmail.com>
2015-11-05 01:12:21 +0400

    Add activity barchart widget view

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,4 +1,9 @@
 class WidgetsController < ApplicationController
+
   def activity_chart
+    name, repo = params[:repository].split('/')
+    @user = User.new(repo: repo, name: name)
+    repository = @user.clone_repo
+    @statistics = ActivityService.new(repository).commits_hash_per_days.to_json
   end
 end
