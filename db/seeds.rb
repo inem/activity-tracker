72c08cab1e7b2a5c9c959cfbc0afdd0d8f441376
V.Kolesnikov <re.vkolesnikov@gmail.com>
2015-11-14 20:12:44 +0300

    Fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,3 @@ users = {
 users.each do |name, repo|
   User.create(name: name, repo: repo)
 end
-
-repos = ["edavis10/redmine", "justCxx/sicp"]
-
-repos.each do |repo|
-  Project.create(repo: repo)
-end
